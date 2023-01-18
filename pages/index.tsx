08381909daffb8f2620baf73c62b247707ad1562
ayohdev <andrew@yohdigital.com>
2023-01-17 23:02:51 -0500

    Added Tailwind CSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,9 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-      Home
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </>
   )
 }
