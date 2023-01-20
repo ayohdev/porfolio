d13dcf82e69550b9a3d60d5325ed99708b181318
ayohdev <andrew@yohdigital.com>
2023-01-20 10:32:24 -0500

    Project Single

--- components/Projects/index.js
+++ components/Projects/index.js
@@ -1,13 +1,22 @@
 import Logo from '@/components/Logos';
+import Link from 'next/link';
 
 const Container = () => (
   <div className="projects mt-10">
     <h2 className="text-2xl font-bold mb-5">Projects</h2>
     <div className="grid grid-cols-2 md:grid-cols-6 gap-1">
-      <Logo logo="sp" width={50} />
-      <Logo logo="sf" width={145} />
-      <Logo logo="pace" width={85} />
-      <Logo logo="uber" width={85} />
+      <Link href="/projects/stackpath/">
+        <Logo logo="sp" width={50} />
+      </Link>
+      <Link href="/projects/statefarm/">
+        <Logo logo="sf" width={145} />
+      </Link>
+      <Link href="/projects/pace/">
+        <Logo logo="pace" width={85} />
+      </Link>
+      <Link href="/projects/uber/">
+        <Logo logo="uber" width={85} />
+      </Link>
     </div>
   </div>
 );
--- pages/projects/stackpath.tsx
+++ pages/projects/stackpath.tsx
@@ -1,14 +1,43 @@
 import Layout from '@/components/Layout';
 import Container from '@/components/Container';
+import Image from 'next/image';
+import sp from '@/assets/projects/placeholder-graphic.png';
+import Link from 'next/link';
+import { AiOutlineArrowLeft } from 'react-icons/ai';
+import SocialIcons from '@/components/SocialIcons';
 
-const Home = () => {
+const StackPath = () => {
   return (
     <Layout>
       <Container>
-        <h1>StackPath</h1>
+        <div className="columns-4 gap-10 flex justify-between items-center">
+          <Link href="/" className="text-white flex">
+            <AiOutlineArrowLeft fill="white" />
+            Back
+          </Link>
+          <h1 className="col-span-2 text-center text-white font-bold text-lg md:text-3xl ">
+            Andrew Hewitt
+          </h1>
+          <SocialIcons />
+        </div>
+
+        <Image
+          src={sp}
+          className="hero"
+          alt="Andrew Hewitt's Work on StackPath"
+          width={1920}
+        />
+        <p className="mt-10 text-white text-lg">
+          Responsible for maintaining, enhancing, and adding new features to
+          StackPath’s marketing sites. Most notable project is migrating their
+          main WWW site from dotCMS to Gatsby JS with a headless CMS. The
+          project took 6 months and saved the company $20,000 a year in hosting
+          and service fees to use dotCMS. Technologies include: React JS, Gatsby
+          JS, SCSS, webpack, Bootstrap, Docker, WordPress, Figma, and Jira.
+        </p>
       </Container>
     </Layout>
   );
 };
 
-export default Home;
+export default StackPath;
